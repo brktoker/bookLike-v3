35763ad2caec7461d59c48c37f64005c4b1b165b
brktoker <brktoker@gmail.com>
2024-01-25 14:47:38 +0300

    re-arrange main js

--- src/main.js
+++ src/main.js
@@ -1,22 +1,24 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import '@/assets/style.css'
-import {appAxios} from '@/utils/appAxios'
-import {socketio} from '@/utils/socketio'
-
+import "@/assets/style.css";
+import BookmarkList from "@/components/BookmarkItems/Index.vue";
 //components
-import AppHeader from '@/components/Shared/AppHeader.vue'
-import BookmarkList from '@/components/BookmarkItems/Index.vue'
-const app = createApp(App)
-app.component("AppHeader",AppHeader)
-app.component("BookmarkList", BookmarkList)
+import AppHeader from "@/components/Shared/AppHeader.vue";
+import { appAxios } from "@/utils/appAxios";
+import { socketio } from "@/utils/socketio";
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+const app = createApp(App);
+app.component("AppHeader", AppHeader);
+app.component("BookmarkList", BookmarkList);
 
 app.config.globalProperties.$axios = appAxios;
 app.config.globalProperties.$socket = socketio;
 
+app.provide("appAxios", appAxios);
+app.provide("socketio", socketio);
 
-app.use(router)
-app.use(store)
-app.mount('#app')
+app.use(router);
+app.use(store);
+app.mount("#app");
